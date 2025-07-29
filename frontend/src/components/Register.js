b669fb59bc7b6e52a71083428c24638de871b0ad
atheesh2 <149477723+atheesh2@users.noreply.github.com>
2025-07-29 14:15:11 +0530

    Update Register.js

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -20,7 +20,7 @@ const Register = () => {
     }
 
     try {
-      const response = await fetch('http://localhost:5000/api/auth/register', {
+      const response = await fetch('https://online-examination-system-uje7.onrender.com/api/auth/register', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
